a47bdbb2dacf258efedaded8a60693973e0d823f
Johannes Zlattinger <johannes@zlattinger.net>
2013-02-01 10:40:19 +0100

    introduced cocoapods

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '6.0'
+
+target :test do
+   link_with 'ZLJCommonsTests'
+
+   pod 'Kiwi'
+end
---
+++ ZLJCommons/ZLJCommons.h
@@ -0,0 +1,9 @@
+//
+//  ZLJCommons.h
+//  ZLJCommons
+//
+//  Created by Johannes Zlattinger on 1/2/13.
+//  Copyright (c) 2013 Johannes Zlattinger. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
