9bf5e330079b0d2cdd111569a48322b9c43ae782
Johannes Zlattinger <johannes@zlattinger.net>
2013-03-07 17:24:53 +0100

    updated singleton macro to accept initialization block

--- ZLJCommons/ZLJCommonMacros.h
+++ ZLJCommons/ZLJCommonMacros.h
@@ -7,11 +7,21 @@
  *      return SINGLETON;
  *  }
  */
-#define SINGLETON ^{\
+#define SINGLETON SINGLETON_WITH_INITIALIZATION(^(id _) {})
+
+/* Usage:
+ *  + (instancetype)sharedInstance {
+ *      return SINGLETON(^(id sharedInstance) {
+ *          // finish setup of shared instance
+ *      });
+ *  }
+ */
+#define SINGLETON_WITH_INITIALIZATION(block) ^{\
     static id sharedInstance; \
     static dispatch_once_t onceToken; \
     dispatch_once(&onceToken, ^{ \
         sharedInstance = [[self alloc] init]; \
+        block(sharedInstance);\
     }); \
     return sharedInstance; \
 }()
