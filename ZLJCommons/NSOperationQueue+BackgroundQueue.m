8c95e82010c0f7d408b457653d96c264b1548b3a
Johannes Zlattinger <johannes@zlattinger.net>
2013-03-07 17:43:51 +0100

    introduced sequential background queue

--- ZLJCommons/NSOperationQueue+BackgroundQueue.m
+++ ZLJCommons/NSOperationQueue+BackgroundQueue.m
@@ -3,15 +3,21 @@
 //
 
 
-#import "NSOperationQueue+BackgroundQueue.h"
-
 #import "ZLJCommonMacros.h"
+
+#import "NSOperationQueue+BackgroundQueue.h"
 
 
 @implementation NSOperationQueue (BackgroundQueue)
 
 + (instancetype)backgroundQueue {
     return SINGLETON;
+}
+
++ (instancetype)sequentialBackgroundQueue {
+    return SINGLETON_WITH_INITIALIZATION(^(NSOperationQueue *queue) {
+        queue.maxConcurrentOperationCount = 1;
+    });
 }
 
 @end
