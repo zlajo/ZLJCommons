1c57cf60a0026e74c492f0d4f675a1880d5cead2
Johannes Zlattinger <johannes@zlattinger.net>
2013-03-07 17:36:52 +0100

    introduced macro to perform task in background and activate a UIActivityIndicatorView while the task is executed

--- ZLJCommons/ZLJCommonMacros.h
+++ ZLJCommons/ZLJCommonMacros.h
@@ -26,3 +26,20 @@
     return sharedInstance; \
 }()
 
+/* Usage:
+ *  PERFORM_IN_BACKGROUND_WITH_ACTIVITY_INDICATION(myActivityIndicatorView, ^{
+ *      // perform background task (be careful with UIKit related stuff!)
+ *  });
+ */
+#define PERFORM_IN_BACKGROUND_WITH_ACTIVITY_INDICATION(activityIndicator_, block_) \
+    ^void (UIActivityIndicatorView *activityIndicator, void (^block)(void)) { \
+        [[NSOperationQueue backgroundQueue] addOperationWithBlock:^{ \
+            [[NSOperationQueue mainQueue] addOperationWithBlock:^{ \
+                [activityIndicator startAnimating]; \
+            }]; \
+            block(); \
+            [[NSOperationQueue mainQueue] addOperationWithBlock:^{ \
+                [activityIndicator stopAnimating]; \
+            }]; \
+        }]; \
+    } (activityIndicator_, block_)
